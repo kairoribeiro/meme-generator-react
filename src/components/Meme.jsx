0397d6c8fd75bcdbe31640222767aef9873d6383
Kairo Ribeiro <kairo10@gmail.com>
2022-10-19 23:28:19 -0400

    add form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,18 @@
 const Meme = () => {
+
+    function handleClick() {
+        console.log('i was clicked');
+    }
+
+
     return ( 
-        <>
-        </>
+        <main>
+            <div className="form">
+                <input type="text" placeholder="Top Text" className="form-input"/>
+                <input type="text" placeholder="Bottom Text" className="form-input"/>
+                <button onClick={handleClick} type="submit" className="form-btn">Get a new Meme Image 🖼</button>
+            </div>
+        </main>
     );
 }
 
